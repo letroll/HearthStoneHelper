7c9e154d85a44391ed4a931ef48f997dadd07b4c
letroll <julien.quievreux@gmail.com>
2014-07-30 15:04:09 +0200

    maj

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 import org.apache.commons.lang3.SystemUtils;
 
-import java.io.File;
+import java.io.*;
 
 /**
  * Created by jquievreux on 28/07/2014.
@@ -54,11 +54,42 @@ public class Main implements Constants{
 
         if(logExist){
             report("log file exist");
-        }else createLogConfigForMac();
+        } else createLogConfigForMac(logConfig);
     }
 
-    private static void createLogConfigForMac() {
+    private static void createLogConfigForMac(File logConfig) {
+        boolean fileCreated = false;
+        try {
+            fileCreated = logConfig.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (fileCreated) {
+            WriteLogFile(logConfig);
+        } else {
+            report("log config file not created");
+        }
+    }
 
+    private static void WriteLogFile(File logConfig) {
+        InputStream stream = Main.class.getResourceAsStream("log.config");
+
+        try {
+            OutputStream os = new FileOutputStream(logConfig);
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            //read from is to buffer
+            while ((bytesRead = stream.read(buffer)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+            stream.close();
+            //flush OutputStream to write any buffered data to file
+            os.flush();
+            os.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            logConfig.deleteOnExit();
+        }
     }
 
     private static void report(String txd){
